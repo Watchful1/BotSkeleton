efea41aa5ec1b420665d1f347f53408f01f47b81
Watchful1 <watchful@watchful.gr>
2017-07-09 17:07:00 -0400

    Finish discord integration

--- main.py
+++ main.py
@@ -7,20 +7,26 @@ import time
 import sys
 import configparser
 import signal
+import discord
+import asyncio
 
 ### Config ###
 LOG_FOLDER_NAME = "logs"
 SUBREDDIT = "default"
 USER_AGENT = "default (by /u/Watchful1)"
-LOOP_TIME = 15*60
+LOOP_TIME = 15
+CONFIG_FILE_NAME = "config.ini"
+DISCORD_OWNER = "Watchful1#8126"
+REDDIT_OWNER = "Watchful1"
 
 ### Logging setup ###
 LOG_LEVEL = logging.DEBUG
 if not os.path.exists(LOG_FOLDER_NAME):
-    os.makedirs(LOG_FOLDER_NAME)
+	os.makedirs(LOG_FOLDER_NAME)
 LOG_FILENAME = LOG_FOLDER_NAME+"/"+"bot.log"
 LOG_FILE_BACKUPCOUNT = 5
 LOG_FILE_MAXSIZE = 1024 * 256
+
 
 log = logging.getLogger("bot")
 log.setLevel(LOG_LEVEL)
@@ -35,9 +41,14 @@ if LOG_FILENAME is not None:
 	log.addHandler(log_fileHandler)
 
 
+async def close_bot(source):
+	await send_message("Bot closed: "+source)
+	quit()
+
+
 def signal_handler(signal, frame):
 	log.info("Handling interupt")
-	sys.exit(0)
+	close_bot("sigint")
 
 signal.signal(signal.SIGINT, signal_handler)
 
@@ -66,13 +77,85 @@ except configparser.NoSectionError:
 	log.error("User "+user+" not in praw.ini, aborting")
 	sys.exit(0)
 
-while True:
-	startTime = time.perf_counter()
-	log.debug("Starting run")
 
-	log.info("Logged into reddit as /u/"+str(r.user.me()))
+async def main():
+	while True:
+		startTime = time.perf_counter()
+		log.debug("Starting run")
 
-	log.debug("Run complete after: %d", int(time.perf_counter() - startTime))
-	if once:
-		break
-	time.sleep(LOOP_TIME)
+		log.info("Logged into reddit as /u/" + str(r.user.me()))
+
+		log.debug("Run complete after: %d", int(time.perf_counter() - startTime))
+		if once:
+			break
+
+		await asyncio.sleep(LOOP_TIME)
+
+
+client = discord.Client()
+botname = "default"
+channel = -1
+config = configparser.ConfigParser()
+config.read(CONFIG_FILE_NAME)
+if "discord" not in config:
+	config.add_section("discord")
+if not config.has_option("discord", "token"):
+	token = input("Enter discord token: ")
+	config.set("discord", "token", token)
+	cfgfile = open(CONFIG_FILE_NAME, 'w')
+	config.write(cfgfile)
+	cfgfile.close()
+	log.debug("Saved bot token")
+
+
+@client.event
+async def on_ready():
+	global botname
+	global channel
+	botname = client.user.name
+	log.debug("Logged into discord as "+botname)
+
+	if "discord" in config and config.has_option("discord", "channel"):
+		channel = client.get_channel(config.get("discord", "channel"))
+		await client.send_message(channel, 'Bot started')
+
+		class DiscordHandler(logging.Handler):
+			def __init__(self):
+				logging.Handler.__init__(self)
+
+			def emit(self, record):
+				asyncio.ensure_future(send_message(record.message), loop=asyncio.get_event_loop())
+
+		logging.handlers.DiscordHandler = DiscordHandler
+		discord_stderrHandler = logging.handlers.DiscordHandler()
+		discord_formatter = logging.Formatter('%(levelname)s: %(message)s')
+		discord_stderrHandler.setFormatter(discord_formatter)
+		log.addHandler(discord_stderrHandler)
+
+
+@client.event
+async def on_message(message):
+	if str(message.author) == DISCORD_OWNER:
+		log.debug(message.content)
+		if message.content == "!activate "+botname:
+			config.set("discord", "channel", str(message.channel.id))
+			cfgfile = open(CONFIG_FILE_NAME, 'w')
+			config.write(cfgfile)
+			cfgfile.close()
+
+			await client.send_message(message.channel, "Activated in channel: "+str(message.channel))
+
+		elif message.content == "!status":
+			await client.send_message(message.channel, "Running")
+
+		elif message.content == "!stop":
+			await close_bot("discord")
+
+
+async def send_message(message):
+	await client.send_message(channel, message)
+
+
+client.loop.create_task(main())
+client.run(config.get("discord","token"))
+
